0e381cb1c1bea9fd126b8835ddd5a33ba33b05cc
Liam Edwards-Playne <liamzebedee@yahoo.com.au>
2013-05-29 19:08:59 +1000

    Added some visual feedback to the (non-functional) webrtc test.

--- src/assets/js/app/tests/webrtc.js
+++ src/assets/js/app/tests/webrtc.js
@@ -1,13 +1,14 @@
 define(['jquery', 'app/net', 'app/bencode'], function($, net, bencode){
-	/*const IP = 'X';
+	/*
+	const IP = 'X';
 	var node = net.connect(IP);
-	net.register('ping', function(msg, args){
-		// msg { 'sender', 'id', respond }
+	net.register('ping', function(msg, args) {
+		$("<li/>").appendTo("#events").html("Message "+args.msg+" from "+msg.sender.toString());​
 		msg.respond({ 'msg': args.msg });
 	});
 	var promise = node.query('ping', {msg: 'hello'});
-	promise.done(function(args) {
-		alert(args.msg);
+	promise.done(function(val) {
+		$("<li/>").appendTo("#events").html("Reply to ping recevied from "+node.toString());​
 	});
 	*/
 });
